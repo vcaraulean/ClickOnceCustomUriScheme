cb9d5f86ab1eeaa5f2fcb82a105041ba1ae1ffdc
Valeriu Caraulean <valeriu@caraulean.com>
2016-02-17 00:09:31 +0100

    Configured logging for easier debugging

--- ClickOnceCustomUriScheme/App.xaml.cs
+++ ClickOnceCustomUriScheme/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
+using NLog;
+using NLog.Fluent;
 
 namespace ClickOnceCustomUriScheme
 {
@@ -13,5 +16,19 @@ namespace ClickOnceCustomUriScheme
     /// </summary>
     public partial class App : Application
     {
+        private readonly Logger _log = LogManager.GetCurrentClassLogger();
+
+        protected override void OnLoadCompleted(NavigationEventArgs e)
+        {
+            base.OnLoadCompleted(e);
+
+            _log.Info("Application load completed");
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _log.Info("Application is starting up");
+        }
     }
 }
